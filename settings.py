2432e25598be839945b0bddf921bfbf61e7dd888
Nicolas Carchio <ncarchio@gmail.com>
2025-01-19 20:54:36 -0500

    addinng settings file to be filled out by each user :lock:

---
+++ settings.py
@@ -0,0 +1,10 @@
+SETTINGS = {
+    "google_sheets_credentials": {
+        # Paste in from the credential that you created and downloaded 
+        # as a json from the google cloud console.
+      },
+      # Your google sheets will look something like: 
+      # https://docs.google.com/spreadsheets/d/<workbook_id>/. 
+      # Take the workbook id and paste it here. 
+      "workbook_id": ""
+}
