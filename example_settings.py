29ae35521a931991d16e46d41b0297421076ac60
Nicolas Carchio <ncarchio@gmail.com>
2025-01-19 21:47:00 -0500

    added example settings :memo:

---
+++ example_settings.py
@@ -0,0 +1,7 @@
+# Rename this file to "settings.py"
+SETTINGS = {
+    # Add your google sheets credentials by downloading the json file.
+    "google_sheets_credentials": {},
+    # Add your workbook id from the url of the google sheet you are looking to work on.
+    "workbook_id": ""
+}
